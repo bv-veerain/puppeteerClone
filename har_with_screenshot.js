5392a6c1d420eff504405d666a45ed7ccb15fc02
bv-akash <akash@blogvault.net>
2018-08-20 12:31:38 +0530

    review changes

--- har_with_screenshot.js
+++ har_with_screenshot.js
@@ -1,11 +1,11 @@
 class CustomError extends Error {
-  constructor ( message, extra ) {
-    super()
-    Error.captureStackTrace( this, this.constructor )
-    this.name = 'CustomError'
-    this.message = message
-    if ( extra ) this.extra = extra
-  }
+	constructor ( message, error_code ) {
+		super()
+		Error.captureStackTrace( this, this.constructor )
+		this.name = 'CustomError'
+		this.message = message
+		if ( error_code ) this.error_code = error_code
+	}
 }
 
 var http = require('http');
@@ -19,32 +19,32 @@ server.on('request',async (req, res) =>
 		{
 			if(req.method == 'GET')
 			{
-				const buffer =  await generate_screenshot_har(req.headers.link);
+				const buffer =  await generate_screenshot_har(req.headers.link, req.headers.proxy_server);
 				var result = JSON.stringify({ har : buffer["har"],
-						site_screenshot : buffer["site_screenshot"],
-						full_site_screenshot : buffer["full_site_screenshot"] });
+					site_screenshot : buffer["site_screenshot"],
+					full_site_screenshot : buffer["full_site_screenshot"] });
 				res.end(result);
-			} 
+			}
 		}
 		catch(err)
 		{
-			if(err.extra === undefined)
-				err.extra = 422;
-			res.writeHead(err.extra);
+			if(err.error_code === undefined)
+				err.error_code = 422;
+			res.writeHead(err.error_code);
 			res.end(err.message);
 		}
 	}).listen(8080);
 
-async function generate_screenshot_har(link)
+async function generate_screenshot_har(link, proxy_server)
 {
 	const browser = await puppeteer.launch({headless: true, slowmo: 0, ignoreHTTPSErrors: true,
-			args: [ '--proxy-server = 127.0.0.1:9876' ]});
+		args: [ '--proxy-server = ' + proxy_server ]});
 	const page = await browser.newPage();
 	await page.setExtraHTTPHeaders({'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'});
 	const har = new PuppeteerHar(page);
 	await har.start();
 	const response = await page.goto(link, { networkIdle2Timeout: 5000, waitUntil: 'load',
-			timeout: 40000 });
+		timeout: 40000 });
 	await page.setViewport({
 		width: 1366,
 		height: 768
@@ -58,5 +58,5 @@ async function generate_screenshot_har(link)
 		error = new CustomError('Puppeteer is Fine. Unable to Load URL '+link, response.status());
 		throw error;
 	}
-	return {har : JSON.stringify(data), site_screenshot : screenshot, full_site_screenshot : fullpagescreenshot};
+	return { har : JSON.stringify(data), site_screenshot : screenshot, full_site_screenshot : fullpagescreenshot };
 }
