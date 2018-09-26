593cb89ff6c0cd84ea846828119af6db66e39cc9
bv-akash <akash@blogvault.net>
2018-09-26 13:26:19 +0530

    #15 Tested Code

--- server.js
+++ server.js
@@ -1,27 +1,81 @@
 'use strict'
 
-const http = require('http')
-const handler = require('./request_handler.js')
+const Hapi = require('hapi')
+const Puppeteer = require('./puppeteer.js')
+const Yslow = require('./yslow.js')
 
-process.setMaxListeners(10)
-
-const server = http.createServer((req, res) => {
-	if (req.method === 'POST') {
-		switch (req.url) {
-		case "/har_and_screenshot":
-			handler.handleGetHarAndScreenshot(req, res)
-			break
-		case "/yslow_report":
-			handler.handlePostYslowReport(req, res)
-			break
-		case "/tokenize":
-			handler.handlePostTokenize(req, res)
-			break
-		default:
-			handler.handleNotFound(req, res)
-		}
-	} else {
-		handler.handleNotFound(req, res)
-	}
+const server = Hapi.server({
+  port: 8080,
+  host: 'localhost'
 })
-server.listen(8080)
+
+server.route({
+  method: 'POST',
+  path: '/har_and_screenshot',
+  handler: async(request, h) => {
+    try {
+      const data = request.payload
+      let har_and_screenshot = await Puppeteer.generateHarAndScreenshot(
+        data.url,
+        data.proxy,
+        data.username,
+        data.password
+      )
+      return (JSON.stringify(har_and_screenshot))
+    } catch (err) {
+      request.log(['HARANDSCREENSHOTERROR'], err.message)
+      return h.response(err.message).code(422)
+    }
+  }
+})
+
+server.route({
+  method: 'POST',
+  path: '/yslow_report',
+  config: {
+    payload: {maxBytes: 1000 * 1000 * 25,
+      parse: true,
+      output: 'file'}
+  },
+  handler: async (request, h) => {
+    try {
+      const data = request.payload
+      return await Yslow.generateReport(data.upload.path)
+    } catch (err) {
+      request.log(['YSLOWERROR'], err.message)
+      return h.response(err.message).code(422)
+    }
+  }
+})
+
+const options = {
+  reporters: {
+    fileReporter: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{ response: '*', error: '*', request: '*' }]
+    },{
+      module: 'good-squeeze',
+      name: 'SafeJson'
+    },{
+      module: 'good-file',
+      args: ['./logs/BVLogs.log']
+    }]
+  }
+}
+
+const registerAndStart = async () => {
+  await server.register({
+    plugin: require('good'),
+    options: options
+  })
+
+  await server.start()
+}
+
+registerAndStart()
+
+process.on('unhandledRejection', (err) => {
+  server.log(['UNHANDLEDREJECTION'], err.message)
+  process.exit(1)
+})
