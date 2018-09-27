c13725c2f62161249677f44c9bc209af1c3d7427
bv-akash <akash@blogvault.net>
2018-09-27 16:39:53 +0530

    #15 review changes

--- server.js
+++ server.js
@@ -5,79 +5,78 @@ const Puppeteer = require('./puppeteer.js')
 const Yslow = require('./yslow.js')
 
 const server = Hapi.server({
-  port: 8080,
-  host: 'localhost'
+	port: 8080,
+	host: 'localhost'
 })
 
 server.route({
-  method: 'POST',
-  path: '/har_and_screenshot',
-  handler: async(request, h) => {
-    try {
-      const data = request.payload
-      let har_and_screenshot = await Puppeteer.generateHarAndScreenshot(
-        data.url,
-        data.proxy,
-        data.username,
-        data.password
-      )
-      return (JSON.stringify(har_and_screenshot))
-    } catch (err) {
-      request.log(['HARANDSCREENSHOTERROR'], err.message)
-      return h.response(err.message).code(422)
-    }
-  }
+	method: 'POST',
+	path: '/har_and_screenshot',
+	handler: async(request, h) => {
+		try {
+			const data = JSON.parse(request.payload)
+			let har_and_screenshot = await Puppeteer.generateHarAndScreenshot(
+				data.url,
+				data.proxy,
+				data.username,
+				data.password
+			)
+			return (JSON.stringify(har_and_screenshot))
+		} catch (err) {
+			request.log(['HARANDSCREENSHOTERROR'], err.message)
+			return h.response(err.message).code(422)
+		}
+	}
 })
 
 server.route({
-  method: 'POST',
-  path: '/yslow_report',
-  config: {
-    payload: {maxBytes: 1000 * 1000 * 25,
-      parse: true,
-      output: 'file'}
-  },
-  handler: async (request, h) => {
-    try {
-      const data = request.payload
-      return await Yslow.generateReport(data.upload.path)
-    } catch (err) {
-      request.log(['YSLOWERROR'], err.message)
-      return h.response(err.message).code(422)
-    }
-  }
+	method: 'POST',
+	path: '/yslow_report',
+	config: {
+		payload: {maxBytes: 1000 * 1000 * 25,
+			parse: true,
+			output: 'file'}
+	},
+	handler: async (request, h) => {
+		try {
+			const data = request.payload
+			return await Yslow.generateReport(data.upload.path)
+		} catch (err) {
+			request.log(['YSLOWERROR'], err.message)
+			return h.response(err.message).code(422)
+		}
+	}
 })
 
 const options = {
-  reporters: {
-    fileReporter: [{
-      module: 'good-squeeze',
-      name: 'Squeeze',
-      args: [{ response: '*', error: '*', request: '*' }]
-    },{
-      module: 'good-squeeze',
-      name: 'SafeJson'
-    },{
-      module: 'good-file',
-      args: ['./logs/BVLogs.log']
-    }]
-  }
+	reporters: {
+		fileReporter: [{
+			module: 'good-squeeze',
+			name: 'Squeeze',
+			args: [{ response: '*', error: '*', request: '*' }]
+		},{
+			module: 'good-squeeze',
+			name: 'SafeJson'
+		},{
+			module: 'good-file',
+			args: ['./logs/BVLogs.log']
+		}]
+	}
 }
 
 const registerAndStart = async () => {
-  await server.register({
-    plugin: require('good'),
-    options: options
-  })
+	await server.register({
+		plugin: require('good'),
+		options: options
+	})
 
-  await server.start()
+	await server.start()
 }
 
 registerAndStart()
 
-process.setMaxListeners(15)
+process.setMaxListeners(1000)
 
 process.on('unhandledRejection', (err) => {
-  server.log(['UNHANDLEDREJECTION'], err.message)
-  process.exit(1)
+	server.log(['UNHANDLEDREJECTION'], err.message)
 })
